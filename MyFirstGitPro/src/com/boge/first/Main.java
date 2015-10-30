7c1f1ebe7f3c02fd6a38a3135a6775ffc96a70a3
songbo <songb@bsfit.com.cn>
2015-10-30 10:38:49 +0800

    hehe

--- MyFirstGitPro/src/com/boge/first/Main.java
+++ MyFirstGitPro/src/com/boge/first/Main.java
@@ -3,6 +3,6 @@ package com.boge.first;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-		System.out.println("这是我的啊，亲！！");
+		System.out.println("这是我的啊，亲！！hahahahaha ");
 	}
 }
