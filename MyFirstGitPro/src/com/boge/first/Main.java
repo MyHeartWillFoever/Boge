230ed9b4a933892665229fb0187bab881d84814f
songbo <songb@bsfit.com.cn>
2015-11-02 10:51:47 +0800

    两个文件冲突，怎么解决？

--- MyFirstGitPro/src/com/boge/first/Main.java
+++ MyFirstGitPro/src/com/boge/first/Main.java
@@ -2,7 +2,7 @@ package com.boge.first;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("2222Hello World!!");
+		System.out.println("22223333Hello World!!");
 		System.out.println("这是我的啊，亲啊洒洒是事实上45555553456！！hahahahaha");
 		System.out.println("这是我的啊，亲啊洒洒是事实上34569876！！hahahahaha");
 		System.out.println("再试试 2345  09876 asasasa");
--- MyFirstGitPro/src/com/boge/first/Test.java
+++ MyFirstGitPro/src/com/boge/first/Test.java
@@ -3,5 +3,6 @@ package com.boge.first;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("wowowowwoowwo");
+		System.out.println("cacacacac");
 	}
 }
