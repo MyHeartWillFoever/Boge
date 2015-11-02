c20767cd5cf28682ada0ec10732f9a02214db258
songbo <songb@bsfit.com.cn>
2015-11-02 11:01:50 +0800

    再次测试冲突

--- MyFirstGitPro/src/com/boge/first/Test.java
+++ MyFirstGitPro/src/com/boge/first/Test.java
@@ -3,6 +3,6 @@ package com.boge.first;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("wowowowwoowwo");
-		System.out.println("heheheheheh");
+		System.out.println("111111heheheheheh");
 	}
 }
