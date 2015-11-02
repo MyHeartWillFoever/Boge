8836bc254c957f7d6eb3a15fbe44382f0dce31e1
songbo <songb@bsfit.com.cn>
2015-11-02 10:48:17 +0800

    冲突吗

--- MyFirstGitPro/src/com/boge/first/Main.java
+++ MyFirstGitPro/src/com/boge/first/Main.java
@@ -2,7 +2,7 @@ package com.boge.first;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("2222Hello World!!");
 		System.out.println("这是我的啊，亲啊洒洒是事实上34569876！！hahahahaha");
 		System.out.println("再试试 2345  09876 asasasa");
 		System.out.println("再试试 2345  6789 asasasa");
