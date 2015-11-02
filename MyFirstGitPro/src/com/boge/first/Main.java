afe46317a75b1f0a5fe57cbf65458df33be2d57f
songbo <songb@bsfit.com.cn>
2015-11-02 10:31:55 +0800

    我我我我我操

--- MyFirstGitPro/src/com/boge/first/Main.java
+++ MyFirstGitPro/src/com/boge/first/Main.java
@@ -3,7 +3,7 @@ package com.boge.first;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-		System.out.println("这是我的啊，亲！！hahahahaha");
+		System.out.println("这是我的啊，23456789亲！！hahahahaha");
 		System.out.println("再试试 2345  09876 asasasa");
 		System.out.println("再试试 2345  6789 asasasa");
 	}
