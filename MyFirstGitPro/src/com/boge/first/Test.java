6baa7cb8f6395e24fd5403cb5d5b362fbc7ed52a
songbo <songb@bsfit.com.cn>
2015-11-03 17:21:29 +0800

    哈哈，就是无聊

--- MyFirstGitPro/src/com/boge/first/Test.java
+++ MyFirstGitPro/src/com/boge/first/Test.java
@@ -1,9 +1,41 @@
 package com.boge.first;
-
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("wowowowwoowwo");
-		System.out.println("222222heheheheheh");
-		System.out.println("呵呵呵222111111heheheheheh");
+		Node n0 = new Node(0);
+		Node n1 = new Node(1);
+		n0.next = n1;
+		Node n2 = new Node(2);
+		n1.next = n2;
+		Node n3 = new Node(3);
+		n2.next = n3;
+		Node n4 = new Node(4);
+		n3.next = n4;
+		Node n5 = new Node(5);
+		n4.next = n5;
+		Node res = reverse(n0);
+		while(res.next!=null){
+			System.out.print(res.value+" ");
+			res = res.next;
+		}
+		System.out.println(res.value);
+	}
+	
+	public static Node reverse(Node node) {
+		Node preNode = null;
+		Node nextNode = null;
+		while(node != null){
+			nextNode = node.next;
+			node.next = preNode;
+			preNode = node;
+			node = nextNode;
+		}
+		return preNode;
+	}
+	private static class Node{
+		int value;
+		Node next;
+		public Node(int value){
+			this.value = value;
+		}
 	}
 }
